585855972d18ffd48310014ab22488510777a212
osazee oboh <osazeeoboh149@gmail.com>
2022-11-10 08:12:28 -0800

    initial commit

---
+++ chapterThree/Account.java
@@ -0,0 +1,36 @@
+public class Account{
+
+    private String name;
+private double balance;
+
+
+  public Account(){
+
+}
+
+  public Account(string name, double balance){
+           this.name = name;
+            this.balance = balance;
+
+     }
+
+     public string getName{
+        return namae;
+     }
+
+
+
+     public void setName(String name){
+
+             this.name = name;
+
+       }
+
+       public void getBalance(double balance){
+
+           this.balance = balance
+
+      }
+
+
+      }
